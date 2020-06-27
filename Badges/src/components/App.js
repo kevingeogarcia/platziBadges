b228c4f6139023545061949eac88ed1e0b137c43
Kevin Hernandez <kevingeogarcia@gmail.com>
2020-06-26 22:23:00 -0600

    add file for yhe server (call data badges!!!)

--- Badges/src/components/App.js
+++ Badges/src/components/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { BrowserRouter,Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Badges from "../pages/Badges";
 import BadgeNew from "../pages/BadgeNew";
-import Layout from './Layout';
-import NotFound from '../pages/NotFound';
-import Home from '../pages/Home';
-
+import Layout from "./Layout";
+import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-      <Switch>
-        <Route exact path="/badges" component={Badges} />
-        <Route exact path="/badges/New" component={BadgeNew} />
-        <Route exact path="/home" component={Home} />
-        <Route component={NotFound}/>
-      </Switch>
+        <Switch>
+          <Redirect from="/" to="home" exact />
+          <Route exact path="/badges" component={Badges} />
+          <Route exact path="/badges/New" component={BadgeNew} />
+          <Route exact path="/home" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
       </Layout>
     </BrowserRouter>
   );
--- Badges/src/components/BadgesList.js
+++ Badges/src/components/BadgesList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './styles/BadgesList.css';
+import {Link } from 'react-router-dom';
 
 class BadgesListItem extends React.Component {
   render() {
@@ -26,6 +27,16 @@ class BadgesListItem extends React.Component {
 
 class BadgesList extends React.Component {
   render() {
+    if(this.props.badges.length === 0){
+      return (
+        <div>
+          <h3>Not Badges were found :(</h3>
+          <Link to='/badges/new' className="btn btn-primary" >
+            Create New Badge
+          </Link>
+        </div>
+      );  
+    }
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
