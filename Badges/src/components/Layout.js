de199c811104c14ca3a50cb3c2caecb0afd2918f
Kevin Hernandez <kevingeogarcia@gmail.com>
2020-07-09 22:06:41 -0600

    <><C:/Program Files/Git/>

--- Badges/src/components/Layout.js
+++ Badges/src/components/Layout.js
@@ -4,10 +4,10 @@ import Navbar from "./Navbar";
 function Layout(props) {
   //const children = props.children;
   return (
-    <React.Fragment>
+    <>
       <Navbar />
       {props.children}
-    </React.Fragment>
+    </>
   );
 }
 
